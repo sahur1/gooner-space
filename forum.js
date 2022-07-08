65991ff08c05d50b88f6ba5d7d0a46bcd8642829
Ritwik Jain Sahu <35268745+sahur1@users.noreply.github.com>
2022-07-08 01:14:59 -0400

    feat: add form validation and upvotes

--- forum.js
+++ forum.js
@@ -2,30 +2,18 @@
 var container = document.querySelector('ol');
 // add default threads
 for (let thread of threads) {
-  var html = `
-    <li class="row">
-      <a href="/thread.html?${thread.id}">
-        <h4 class="title">
-          ${thread.title}
-        </h4>
-        <div class="bottom">
-          <p class="timestamp">
-            ${new Date(thread.date).toLocaleString()}
-          </p>
-          <p class="comment-count">
-            ${thread.comments.length} comments
-          </p>
-        </div>
-      </a>
-    </li>
-  `;
-  container.insertAdjacentHTML('beforeend', html);
+  addThread(thread);
 }
 
 // write thread html using thread object
 function addThread(thread) {
   var threadHtml = `
     <li class="row">
+      <div id="${thread.id}" class="upvotejs">
+        <a class="upvote"></a>
+        <span class="count">0</span>
+        <a class="downvote"></a>
+      </div>
       <a href="/thread.html?${thread.id}">
         <h4 class="title">
           ${thread.title}
@@ -42,11 +30,11 @@ function addThread(thread) {
     </li>
   `;
   container.insertAdjacentHTML('afterbegin', threadHtml);
+  Upvote.create(thread.id);
 }
 
 // add event listener on thread submit button
-var btn = document.getElementById('add-thread');
-btn.addEventListener('click', function() {
+function insertThread() {
   // create thread object with user input
   var txt = document.querySelector('textarea');
   var thread = {
@@ -62,4 +50,28 @@ btn.addEventListener('click', function() {
   txt.value='';
   threads.unshift(thread);
   localStorage.setItem('threads', JSON.stringify(threads));
+}
+
+$(function(){
+  $("form[name=threadcontent]").validate({
+    rules: {
+      threadcontent: "required"
+    },
+    messages: {
+      threadcontent: "Please enter your submission"
+    },
+    submitHandler: function(){
+      insertThread();
+      return false;
+    }
+  });
+
+  $("form[name=location]").validate({
+    rules: {
+      location: "required"
+    },
+    messages: {
+      location: "Please enter a valid address"
+    }
+  });
 });
